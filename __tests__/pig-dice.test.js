b87985a7c703ec5341adb64104259c5d9104ce0b
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-13 20:57:13 -0800

    attempt to run a good fail on test for rollDice function.

---
+++ __tests__/pig-dice.test.js
@@ -0,0 +1,9 @@
+import diceRoll from './../src/index.js';
+import GameState from './../src/index.js';
+
+describe('diceRoll', () => {
+
+  test('should generate a random number', () => {
+    let diceRoll = new diceRoll();
+  })
+})
